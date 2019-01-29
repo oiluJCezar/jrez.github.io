860947cfa0469823a6eddadf89bef537fcd3622a
oiluJCezar <julio.cezar.resende@gmail.com>
2019-01-29 05:59:59 -0200

    third commit

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -500,8 +500,6 @@
     });
 
 
-
-
     /**
      * =======================================
      * testimonial SYNC WITH CLIENTS
@@ -518,96 +516,6 @@
       pagination:         true,
       // transitionStyle:    "fade"
     });
-
-
-
-
-
-    /**
-     * =======================================
-     * Contact Form Style
-     * =======================================
-     */
-    $( '.form-control' ).each( function( inputEl ) {
-
-      // in case the input is already filled..
-      if( $(this).val() !== '' ) {
-        $(this).closest('.input-outer').addClass('input-filled');
-      }
-
-      // events:
-      $( this ).focus(function(){
-
-        $(this).closest('.input-outer').addClass('input-filled');
-
-      });
-
-      $( this ).blur(function(){
-
-          if( $(this).val() === '' ) {
-            $(this).closest('.input-outer').removeClass('input-filled');
-          }
-
-      });
-
-    });
-
-
-
-
-
-    /**
-     * ============================
-     * CONTACT FORM 2
-     * ============================
-    */
-    $("#contact-form").on('submit', function(e) {
-      e.preventDefault();
-      var success = $(this).find('.email-success'),
-        failed = $(this).find('.email-failed'),
-        loader = $(this).find('.email-loading'),
-        postUrl = $(this).attr('action');
-
-      var data = {
-        name: $(this).find('.contact-name').val(),
-        email: $(this).find('.contact-email').val(),
-        company: $(this).find('.contact-company').val(),
-        subject: $(this).find('.contact-subject').val(),
-        message: $(this).find('.contact-message').val()
-      };
-
-      if ( isValidEmail(data['email']) && (data['message'].length > 1) && (data['name'].length > 1) ) {
-        $.ajax({
-          type: "POST",
-          url: postUrl,
-          data: data,
-          beforeSend: function() {
-            loader.fadeIn(1000);
-          },
-          success: function(data) {
-            loader.fadeOut(1000);
-            success.delay(500).fadeIn(1000);
-            failed.fadeOut(500);
-          },
-          error: function(xhr) { // if error occured
-            loader.fadeOut(1000);
-            failed.delay(500).fadeIn(1000);
-            success.fadeOut(500);
-          },
-          complete: function() {
-            loader.fadeOut(1000);
-          }
-        });
-      } else {
-        loader.fadeOut(1000);
-        failed.delay(500).fadeIn(1000);
-        success.fadeOut(500);
-      }
-
-      return false;
-    });
-
-
 
   });
 
